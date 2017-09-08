1622a44ef3fc2ad4c2bd03bdc17d53a6f4cdff27
Johnattan Fabian Gutierrez <johnattan.gutierrez@flex.com>
2017-09-08 09:15:22 -0700

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: 'http://tririga-support.flextronics.com/index.html',
+    startURL: 'http://tririga-support.flextronics.com',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
     // startMethod: "login.onStart",
